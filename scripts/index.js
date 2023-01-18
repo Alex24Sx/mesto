82cd1ec36eed8a14b08305be2261fb49dc27e62c
Alex24Sx <alexkobe248@gmail.com>
2023-01-18 21:07:39 +0300

    first script

---
+++ scripts/index.js
@@ -0,0 +1,14 @@
+const editButton = document.querySelector('.profile__edit-button');
+const editPopup = document.querySelector('.popup_active');
+
+editButton.addEventListener('click', (Event) => {
+  Event.preventDefault();
+  editPopup.classList.toggle('popup_opened')
+});
+
+const closeButton = document.querySelector('.popup__close');
+
+closeButton.addEventListener('click', (Event) => {
+  Event.preventDefault();
+  editPopup.classList.toggle('popup_opened')
+});
